c6f81e877c04f88302ec3e0023419e5ca56aca5a
GOAT095 <hakimoxxx@gmail.com>
2022-06-15 20:28:30 +0100

    create task is working get taskbyid causes internal server error

--- app.module.ts
+++ app.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { TasksController } from './tasks/tasks.controller';
+import { TasksService } from './tasks/tasks.service';
+import { TaskRepository } from './tasks/task.repository';
 
 @Module({
   imports: [TasksModule, 
     TypeOrmModule.forRoot(typeOrmConfig),
-    TasksModule,
   ],
+  controllers: [TasksController],
+  providers: [TasksService, TaskRepository],
+
 })
 export class AppModule {}
